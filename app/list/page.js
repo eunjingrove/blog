a1417c341a27754d5272b890c80f080f0272912c
은진 <ok94777@naver.com>
2025-02-26 23:25:36 +0900

    useRouter, prefetch를 통한 상세페이지 만들기

--- app/list/page.js
+++ app/list/page.js
@@ -1,17 +1,19 @@
 import { connectDB } from "@/util/database"
 import Link from "next/link"
+import DetailLink from "./DetailLink"
 
 export default async function List() {
     const db = (await connectDB).db('blog')
     let result = await db.collection('post').find().toArray()
-    console.log(result)
 
     return (
       <div className="list-bg">
         {result.map((v,i) => (
             <div key={i} className="list-item">
-                <h4>{v.title}</h4>
-                <Link href={'/detail/'+ v._id}>링크</Link>
+                <Link href={'/detail/'+ v._id}>
+                  <h4>{v.title}</h4>
+                </Link>
+                <DetailLink />
                 <p>{v.content}</p>
             </div>
         ))}
