91cb6b087f58de6158bce467322fab582c9be6fa
은진 <ok94777@naver.com>
2025-03-04 23:04:17 +0900

    글 작성페이지 생성

---
+++ app/pages/api/post/new.js
@@ -0,0 +1,15 @@
+import { connectDB } from "@/util/database";
+
+export default async function handler(요청, 응답) {
+    if (요청.method == 'POST') {
+        console.log(요청.body)
+        if(요청.body.title == '') {
+            return 응답.status(500).json('제목을 작성해주세요.')
+        } else {
+            const db = (await connectDB).db("blog")
+            let result = await db.collection('post').insertOne(요청.body)
+            return 응답.status(200).redirect('/list')
+        }
+        
+    }
+}
