e5733661d5777840244a1550abc727e444405806
은진 <ok94777@naver.com>
2025-03-25 23:57:49 +0900

    DB adapter 설정

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -8,6 +10,7 @@ export const authOptions = {
       clientSecret: '40e12fe24c311d1428a9cac25e3b54930b56cdbe',
     }),
   ],
-  secret : 'ghwn0wnrnjs'
+  secret : 'ghwn0wnrnjs',
+  adapter : MongoDBAdapter(connectDB)
 };
 export default NextAuth(authOptions); 
