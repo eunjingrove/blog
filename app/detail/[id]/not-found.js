d337fea5ee51a7144e74ae9d5d4f036d90222e85
eunjin.Park <ok94777@naver.com>
2025-04-03 22:50:29 +0900

    로딩, 에러, 404 페이지

---
+++ app/detail/[id]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 - 페이지를 찾을 수 없습니다.</h1>
+            <a href="/">홈으로 돌아가기</a>
+        </div>
+    );
+}
--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -6,6 +6,10 @@ export default async function Detail({ params }) {
     const db = (await connectDB).db("blog");
     let result = await db.collection("post").findOne({ _id: new ObjectId(params.id) });
 
+    if (result === null) {
+        return <div> 404 없는 페이지 </div>
+    }
+
     return (
         <div>
             <h4>상세페이지</h4>
